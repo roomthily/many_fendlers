2b01e64430358f10efbfe5654a3cfea8efa7849b
roomthily <sorenscott@gmail.com>
2016-04-16 23:19:05 -0600

    init commit
    sorting out the corpus

---
+++ data/vernacular.sql
@@ -0,0 +1,8 @@
+﻿-- select distinct(vernacular_name) 
+-- from vernaculars
+-- order by vernacular_name;
+
+select t.tsn, t.unit_name1, t.unit_name2, v.vernacular_name
+from taxonomic_units t join vernaculars v on v.tsn = t.tsn
+where t.kingdom_id = 3
+order by v.vernacular_name;
