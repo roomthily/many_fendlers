b72b3b1172890371cd723dcd54e1ea7568d5e3d5
roomthily <sorenscott@gmail.com>
2016-04-18 08:42:10 -0600

    real-time design in ipy

---
+++ fendler.py
@@ -0,0 +1,23 @@
+"""Whatever."""
+import pandas as pd
+
+
+def check_for_fendler(term):
+    """Check the generated term against the vernacular names.
+
+    return the taxon if found.
+    """
+    pass
+
+
+def generate_response():
+    """NO."""
+    pass
+
+
+def main():
+    """OMG."""
+    pass
+
+if __name__ == '__main__':
+    main()
